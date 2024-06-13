3174a7c0f901e9f02d40a3d29c1caa9a2fadcb79
devarshini36 <devarshinirangineni36@gmail.com>
2024-06-13 17:35:29 +0530

    deleted bookroom

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Contact from "./Pages/Contact";
 import Payment from './Pages/Payment';
 import Book from "./Pages/Book";
 import Form from "./Pages/Form";
-import BookRoom from "./Pages/BookRoom";
+
 import NavBar from './Pages/NavBar';
 
 
@@ -19,11 +19,11 @@ function App() {
       
         <NavBar />
         <Routes>
-          /*<Route path="/" element={<Home/>} /> 
+          <Route path="/" element={<Home/>} /> 
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/book" element={<Book />} />
           <Route path="/form" element={<Form />} />
-          <Route path="/bookroom" element={<BookRoom />} />
+          
           <Route path="/payment" element={<Payment />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
