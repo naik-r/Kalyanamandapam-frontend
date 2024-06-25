bddcf37a486e9071ed7032e5b3b7a30057c56f21
devarshini <devarshinirangineni36@gmail.com>
2024-06-25 10:50:41 +0530

    home change

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -41,7 +41,7 @@ function Home(){
   }, []);
 
     return(
-        <div style={{backgroundColor:'#f0f0f0'}}>
+<div style={{backgroundColor:'#f0f0f0'}}>
 <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
 <div className="carousel-indicators" id="Home">
   <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
